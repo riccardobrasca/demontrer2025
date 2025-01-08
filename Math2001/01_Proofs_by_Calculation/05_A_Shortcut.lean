33963fad7d2b1ee2b35b8fcf7d32040b159372e9
Riccardo Brasca <riccardo.brasca@gmail.com>
2025-01-08 11:05:49 +0100

    remove

--- Math2001/01_Proofs_by_Calculation/05_A_Shortcut.lean
+++ Math2001/01_Proofs_by_Calculation/05_A_Shortcut.lean
@@ -20,15 +20,6 @@ example {x y : ℚ} (hx : x = 2) (hy : y ^ 2 = -7) : x + y ^ 2 = -5 := by
     _ = -5 := by addarith [hx]
   done
 
-example {s t : ℝ} (h : t = 4 - s * t) : t + s * t > 0 := by
-  addarith [h]
-  done
-
-example {m n : ℝ} (h1 : m ≤ 8 - n) : 10 > m + n := by
-  addarith [h1]
-  done
-
-
 -- Check that `addarith` can't verify this deduction!
 example {w : ℚ} (h1 : 3 * w + 1 = 4) : w = 1 := by
   sorry
