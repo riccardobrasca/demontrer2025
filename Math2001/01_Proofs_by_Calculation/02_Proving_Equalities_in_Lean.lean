a08df13704ac44e4888bf8fe1964272c156d97cb
Riccardo Brasca <riccardo.brasca@gmail.com>
2025-01-08 11:13:25 +0100

    remove

--- Math2001/01_Proofs_by_Calculation/02_Proving_Equalities_in_Lean.lean
+++ Math2001/01_Proofs_by_Calculation/02_Proving_Equalities_in_Lean.lean
@@ -30,17 +30,6 @@ example {r s : ℝ} (h1 : s = 3) (h2 : r + 2 * s = -1) : r = -7 := by
     _ = -7 := by sorry
   done
 
--- Example 1.2.3
--- Exercise: replace the words "sorry" with the correct Lean justification.
-example {a b m n : ℤ} (h1 : a * m + b * n = 1) (h2 : b ^ 2 = 2 * a ^ 2) :
-    (2 * a * n + b * m) ^ 2 = 2 := by
-  calc
-    (2 * a * n + b * m) ^ 2
-      = 2 * (a * m + b * n) ^ 2 + (m ^ 2 - 2 * n ^ 2) * (b ^ 2 - 2 * a ^ 2) := by sorry
-    _ = 2 * 1 ^ 2 + (m ^ 2 - 2 * n ^ 2) * (2 * a ^ 2 - 2 * a ^ 2) := by sorry
-    _ = 2 := by sorry
-  done
-
 -- Example 1.2.4.
 -- Exercise: type out the whole proof printed in the text as a Lean proof.
 example {a b c d e f : ℤ} (h1 : a * d = b * c) (h2 : c * f = d * e) :
