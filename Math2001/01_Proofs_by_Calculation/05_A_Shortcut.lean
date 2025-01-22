5957c8e7b0b4f7496c091a8f3d7ba4dcefc66a03
Riccardo Brasca <riccardo.brasca@gmail.com>
2025-01-22 11:00:47 +0100

    better

--- Math2001/01_Proofs_by_Calculation/05_A_Shortcut.lean
+++ Math2001/01_Proofs_by_Calculation/05_A_Shortcut.lean
@@ -11,18 +11,20 @@ example {x : ℤ} (h1 : x + 4 = 2) : x = -2 := by
   done
 
 example {a b : ℤ} (ha : a - 2 * b = 1) : a = 2 * b + 1 := by
-  addarith [ha]
+  sorry
   done
 
 example {x y : ℚ} (hx : x = 2) (hy : y ^ 2 = -7) : x + y ^ 2 = -5 := by
-  calc
-    x + y ^ 2 = x - 7 := by addarith [hy]
-    _ = -5 := by addarith [hx]
+  sorry
   done
 
-example {s t : ℝ} (h : t = 4 - s * t) : t + s * t > 0 := by addarith [h]
+example {s t : ℝ} (h : t = 4 - s * t) : t + s * t > 0 := by
+  addarith [h]
+  done
 
-example {m n : ℝ} (h1 : m ≤ 8 - n) : 10 > m + n := by addarith [h1]
+example {m n : ℝ} (h1 : m ≤ 8 - n) : 10 > m + n := by
+  sorry
+  done
 
 -- Vérifiez que `addarith` ne peut pas vérifier cette déduction
 example {w : ℚ} (h1 : 3 * w + 1 = 4) : w = 1 := by
