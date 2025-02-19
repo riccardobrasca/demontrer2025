92722c0e59c906391d02b4b90446021e03f9a5c5
Riccardo Brasca <riccardo.brasca@gmail.com>
2025-02-19 15:14:48 +0100

    solution

--- Math2001/02_Proofs_with_Structure/05_Exists.lean
+++ Math2001/02_Proofs_with_Structure/05_Exists.lean
@@ -4,6 +4,7 @@ import Library.Basic
 
 math2001_init
 
+-- le_or_gt (x y : ℝ) : x ≤ y ∨ x > y
 
 example {a : ℚ} (h : ∃ b : ℚ, a = b ^ 2 + 1) : a > 0 := by
   obtain ⟨b, hb⟩ := h
@@ -22,7 +23,14 @@ example {t : ℝ} (h : ∃ a : ℝ, a * t < 0) : t ≠ 0 := by
     cancel -x at hxt'
     apply ne_of_gt
     apply hxt'
-  · sorry
+  · have hxt' : x * t < x * 0 := by
+      calc
+        x*t < 0 := by addarith [hxt]
+        _ = x*0 := by ring
+    have hx' : x ≥ 0 := by addarith [hx]
+    cancel x at hxt'
+    apply ne_of_lt
+    apply hxt'
   done
 
 example : ∃ n : ℤ, 12 * n = 84 := by
@@ -36,15 +44,26 @@ example (x : ℝ) : ∃ y : ℝ, y > x := by
   done
 
 example : ∃ m n : ℤ, m ^ 2 - n ^ 2 = 11 := by
-  sorry
+  use 6
+  use 5
+  numbers
   done
 
 example (a : ℤ) : ∃ m n : ℤ, m ^ 2 - n ^ 2 = 2 * a + 1 := by
-  sorry
+  use a+1
+  use a
+  ring
   done
 
 example {p q : ℝ} (h : p < q) : ∃ x, p < x ∧ x < q := by
-  sorry
+  use (p+q)/2
+  constructor
+  · calc
+      p = (p+p)/2 := by ring
+      _ < (p+q)/2 := by rel [h]
+  · calc
+      (p+q)/2 < (q+q)/2 := by rel [h]
+      _ = q := by ring
   done
 
 example : ∃ a b c d : ℕ,
@@ -63,31 +82,73 @@ example : ∃ a b c d : ℕ,
 
 
 example : ∃ t : ℚ, t ^ 2 = 1.69 := by
-  sorry
+  use 1.3
+  numbers
   done
 
 example : ∃ m n : ℤ, m ^ 2 + n ^ 2 = 85 := by
-  sorry
+  use 2, 9
+  numbers
   done
 
 example : ∃ x : ℝ, x < 0 ∧ x ^ 2 < 1 := by
-  sorry
+  use -0.5
+  constructor
+  · numbers
+  · numbers
   done
 
 example : ∃ a b : ℕ, 2 ^ a = 5 * b + 1 := by
-  sorry
+  use 4, 3
+  numbers
   done
 
 example (x : ℚ) : ∃ y : ℚ, y ^ 2 > x := by
-  sorry
+  use x+1/2
+  calc
+    (x+1/2)^2 = x^2 + 1/4 + x := by ring
+    _ > x := by extra
   done
 
 example {t : ℝ} (h : ∃ a : ℝ, a * t + 1 < a + t) : t ≠ 1 := by
-  sorry
+  obtain ⟨a, ha⟩ := h
+  have h1 : a*t-t < a-1 := by addarith [ha]
+  have h2 : t*(a-1) < 1*(a-1) := by
+    calc
+      t*(a-1) = a*t-t := by ring
+      _ < a-1 := h1
+      _ = 1*(a-1) := by ring
+  have h3 := le_or_gt 1 a
+  obtain hale | hagt := h3
+  · have ha1 : a-1 ≥ 0 := by addarith [hale]
+    cancel a-1 at h2
+    apply ne_of_lt
+    assumption
+  · have ha1 : 1-a > 0 := by addarith [hagt]
+    have ha2 : 1*(1-a) < t*(1-a) := by
+      calc
+        1*(1-a) = -(1*(a-1)) := by ring
+        _ < -(t*(a-1)) := by rel [h2]
+        _ = t*(1-a) := by ring
+    cancel 1-a at ha2
+    apply ne_of_gt
+    assumption
   done
 
 example {m : ℤ} (h : ∃ a, 2 * a = m) : m ≠ 5 := by
-  sorry
+  obtain ⟨a, ha⟩ := h
+  have h1 := le_or_succ_le a 2
+  obtain h1 | h1 := h1
+  · apply ne_of_lt
+    calc
+      m = 2 * a := by rw [ha]
+      _ ≤ 2 * 2 := by rel [h1]
+      _ < 5 := by numbers
+  · apply ne_of_gt
+    calc
+      m = 2 * a := by rw [ha]
+      _ ≥ 2 * 3 := by rel [h1]
+      _ > 5 := by numbers
   done
 
 example {n : ℤ} : ∃ a, 2 * a ^ 3 ≥ n * a + 7 := by
