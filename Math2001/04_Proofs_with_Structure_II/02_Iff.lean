be08db4ab41a4073271f7853b606371f96d018a5
Riccardo Brasca <riccardo.brasca@gmail.com>
2025-02-28 14:10:11 +0100

    better

--- Math2001/04_Proofs_with_Structure_II/02_Iff.lean
+++ Math2001/04_Proofs_with_Structure_II/02_Iff.lean
@@ -19,45 +19,6 @@ example {a : ℚ} : 3 * a + 1 ≤ 7 ↔ a ≤ 2 := by
       _ = 7 := by numbers
   done
 
-
-example {n : ℤ} : 8 ∣ 5 * n ↔ 8 ∣ n := by
-  constructor
-  · intro hn
-    obtain ⟨a, ha⟩ := hn
-    use -3 * a + 2 * n
-    calc
-      n = -3 * (5 * n) + 16 * n := by ring
-      _ = -3 * (8 * a) + 16 * n := by rw [ha]
-      _ = 8 * (-3 * a + 2 * n) := by ring
-  · intro hn
-    obtain ⟨a, ha⟩ := hn
-    use 5 * a
-    calc 5 * n = 5 * (8 * a) := by rw [ha]
-      _ = 8 * (5 * a) := by ring
-  done
-
-theorem odd_iff_modEq (n : ℤ) : Odd n ↔ n ≡ 1 [ZMOD 2] := by
-  constructor
-  · intro h
-    obtain ⟨k, hk⟩ := h
-    dsimp [Int.ModEq]
-    dsimp [(· ∣ ·)]
-    use k
-    addarith [hk]
-  · sorry
-  done
-
-theorem even_iff_modEq (n : ℤ) : Even n ↔ n ≡ 0 [ZMOD 2] := by
-  constructor
-  · intro h
-    obtain ⟨k, hk⟩ := h
-    dsimp [Int.ModEq]
-    dsimp [(· ∣ ·)]
-    use k
-    addarith [hk]
-  · sorry
-  done
-
 example {x : ℝ} : x ^ 2 + x - 6 = 0 ↔ x = -3 ∨ x = 2 := by
   sorry
   done
@@ -66,51 +27,10 @@ example {a : ℤ} : a ^ 2 - 5 * a + 5 ≤ -1 ↔ a = 2 ∨ a = 3 := by
   sorry
   done
 
-example {n : ℤ} (hn : n ^ 2 - 10 * n + 24 = 0) : Even n := by
-  have hn1 :=
-    calc (n - 4) * (n - 6) = n ^ 2 - 10 * n + 24 := by ring
-      _ = 0 := hn
-  have hn2 := eq_zero_or_eq_zero_of_mul_eq_zero hn1
-  sorry
-  done
-
-example {n : ℤ} (hn : n ^ 2 - 10 * n + 24 = 0) : Even n := by
-  have hn1 :=
-    calc (n - 4) * (n - 6) = n ^ 2 - 10 * n + 24 := by ring
-      _ = 0 := hn
-  rw [mul_eq_zero] at hn1 -- `hn1 : n - 4 = 0 ∨ n - 6 = 0`
-  sorry
-  done
-
-example {x y : ℤ} (hx : Odd x) (hy : Odd y) : Odd (x + y + 1) := by
-  rw [Int.odd_iff_modEq] at *
-  calc x + y + 1 ≡ 1 + 1 + 1 [ZMOD 2] := by rel [hx, hy]
-    _ = 2 * 1 + 1 := by ring
-    _ ≡ 1 [ZMOD 2] := by extra
-example (n : ℤ) : Even n ∨ Odd n := by
-  mod_cases hn : n % 2
-  · left
-    rw [Int.even_iff_modEq]
-    apply hn
-  · sorry
-  done
-
 /-! # Exercises -/
 
 
 example {x : ℝ} : 2 * x - 1 = 11 ↔ x = 6 := by
-  sorry
-  done
-
-example {n : ℤ} : 63 ∣ n ↔ 7 ∣ n ∧ 9 ∣ n := by
-  sorry
-  done
-
-theorem dvd_iff_modEq {a n : ℤ} : n ∣ a ↔ a ≡ 0 [ZMOD n] := by
-  sorry
-  done
-
-example {a b : ℤ} (hab : a ∣ b) : a ∣ 2 * b ^ 3 - b ^ 2 + 3 * b := by
   sorry
   done
 
