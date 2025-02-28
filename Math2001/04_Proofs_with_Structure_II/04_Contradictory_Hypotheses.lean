30c6644e0c60414af3be029ac6ebda77af88f774
Riccardo Brasca <riccardo.brasca@gmail.com>
2025-02-28 14:48:36 +0100

    improve

--- Math2001/04_Proofs_with_Structure_II/04_Contradictory_Hypotheses.lean
+++ Math2001/04_Proofs_with_Structure_II/04_Contradictory_Hypotheses.lean
@@ -5,6 +5,7 @@ import Library.Tactic.ModEq
 
 math2001_init
 
+-- mul_eq_zero {a b : ℝ} : a * b = 0 ↔ a = 0 ∨ b = 0
 
 example {y : ℝ} (x : ℝ) (h : 0 < x * y) (hx : 0 ≤ x) : 0 < y := by
   obtain hneg | hpos : y ≤ 0 ∨ 0 < y := le_or_lt y 0
@@ -36,52 +37,6 @@ example {t : ℤ} (h2 : t < 3) (h : t - 1 = 6) : t = 13 := by
   numbers at H -- this is a contradiction!
   done
 
-example (n : ℤ) (hn : n ^ 2 + n + 1 ≡ 1 [ZMOD 3]) :
-    n ≡ 0 [ZMOD 3] ∨ n ≡ 2 [ZMOD 3] := by
-  mod_cases h : n % 3
-  · -- case 1: `n ≡ 0 [ZMOD 3]`
-    left
-    apply h
-  · -- case 2: `n ≡ 1 [ZMOD 3]`
-    have H :=
-      calc 0 ≡ 0 + 3 * 1 [ZMOD 3] := by extra
-      _ = 1 ^ 2 + 1 + 1 := by numbers
-      _ ≡ n ^ 2 + n + 1 [ZMOD 3] := by rel [h]
-      _ ≡ 1 [ZMOD 3] := hn
-    numbers at H -- contradiction!
-  · -- case 3: `n ≡ 2 [ZMOD 3]`
-    right
-    apply h
-  done
-
-example {p : ℕ} (hp : 2 ≤ p) (H : ∀ m : ℕ, 1 < m → m < p → ¬m ∣ p) : Prime p := by
-  constructor
-  · apply hp -- show that `2 ≤ p`
-  intro m hmp
-  have hp' : 0 < p := by extra
-  have h1m : 1 ≤ m := Nat.pos_of_dvd_of_pos hmp hp'
-  obtain hm | hm_left : 1 = m ∨ 1 < m := eq_or_lt_of_le h1m
-  · -- the case `m = 1`
-    left
-    addarith [hm]
-  -- the case `1 < m`
-  sorry
-  done
-
-example : Prime 5 := by
-  apply prime_test
-  · numbers
-  intro m hm_left hm_right
-  apply Nat.not_dvd_of_exists_lt_and_lt
-  interval_cases m
-  · use 2
-    constructor <;> numbers
-  · use 1
-    constructor <;> numbers
-  · use 1
-    constructor <;> numbers
-  done
-
 example {a b c : ℕ} (ha : 0 < a) (hb : 0 < b) (hc : 0 < c)
     (h_pyth : a ^ 2 + b ^ 2 = c ^ 2) : 3 ≤ a := by
   sorry
@@ -95,25 +50,11 @@ example {x y : ℝ} (n : ℕ) (hx : 0 ≤ x) (hn : 0 < n) (h : y ^ n ≤ x ^ n) 
   sorry
   done
 
-example (n : ℤ) (hn : n ^ 2 ≡ 4 [ZMOD 5]) : n ≡ 2 [ZMOD 5] ∨ n ≡ 3 [ZMOD 5] := by
-  sorry
-  done
-
-example : Prime 7 := by
-  sorry
-  done
-
 example {x : ℚ} (h1 : x ^ 2 = 4) (h2 : 1 < x) : x = 2 := by
   have h3 :=
     calc
       (x + 2) * (x - 2) = x ^ 2 + 2 * x - 2 * x - 4 := by ring
       _ = 0 := by addarith [h1]
   rw [mul_eq_zero] at h3
-  sorry
-  done
-
-namespace Nat
-
-example (p : ℕ) (h : Prime p) : p = 2 ∨ Odd p := by
   sorry
   done
