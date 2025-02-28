cc9cd6d8bb7a372171745e38c2615955cb45790d
Riccardo Brasca <riccardo.brasca@gmail.com>
2025-02-28 17:27:25 +0100

    start

---
+++ Math2001/04_Proofs_with_Structure_II/05_Proof_by_Contradiction.lean
@@ -0,0 +1,44 @@
+/- Copyright (c) Heather Macbeth, 2023.  All rights reserved. -/
+import Mathlib.Data.Real.Basic
+import Library.Theory.ParityModular
+import Library.Basic
+import Library.Tactic.ModEq
+
+math2001_init
+
+open Int
+
+
+example : ¬ (∀ x : ℝ, x ^ 2 ≥ x) := by
+  intro h
+  have : 0.5 ^ 2 ≥ 0.5 := h 0.5
+  numbers at this
+  done
+
+example {x y : ℝ} (h : x + y = 0) : ¬(x > 0 ∧ y > 0) := by
+  intro h
+  obtain ⟨hx, hy⟩ := h
+  have H :=
+  calc 0 = x + y := by rw [h]
+    _ > 0 := by extra
+  numbers at H
+  done
+
+example : ¬ (∃ n : ℕ, n ^ 2 = 2) := by
+  sorry
+  done
+
+/-! # Exercises -/
+
+
+example : ¬ (∃ t : ℝ, t ≤ 4 ∧ t ≥ 5) := by
+  sorry
+  done
+
+example : ¬ (∃ a : ℝ, a ^ 2 ≤ 8 ∧ a ^ 3 ≥ 30) := by
+  sorry
+  done
+
+example {x : ℝ} (hx : x ^ 2 < 9) : ¬ (x ≤ -3 ∨ x ≥ 3) := by
+  sorry
+  done
