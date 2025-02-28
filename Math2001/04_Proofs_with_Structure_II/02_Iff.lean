29bc5e797a6234df34428a0998664e455fa8da9b
Riccardo Brasca <riccardo.brasca@gmail.com>
2025-02-28 14:10:27 +0100

    add this

---
+++ Math2001/04_Proofs_with_Structure_II/02_Iff.lean
@@ -0,0 +1,39 @@
+/- Copyright (c) Heather Macbeth, 2023.  All rights reserved. -/
+import Mathlib.Data.Real.Basic
+import Library.Basic
+import Library.Tactic.ModEq
+
+math2001_init
+
+namespace Int
+
+
+example {a : ℚ} : 3 * a + 1 ≤ 7 ↔ a ≤ 2 := by
+  constructor
+  · intro h
+    calc a = ((3 * a + 1) - 1) / 3 := by ring
+      _ ≤ (7 - 1) / 3 := by rel [h]
+      _ = 2 := by numbers
+  · intro h
+    calc 3 * a + 1 ≤ 3 * 2 + 1 := by rel [h]
+      _ = 7 := by numbers
+  done
+
+example {x : ℝ} : x ^ 2 + x - 6 = 0 ↔ x = -3 ∨ x = 2 := by
+  sorry
+  done
+
+example {a : ℤ} : a ^ 2 - 5 * a + 5 ≤ -1 ↔ a = 2 ∨ a = 3 := by
+  sorry
+  done
+
+/-! # Exercises -/
+
+
+example {x : ℝ} : 2 * x - 1 = 11 ↔ x = 6 := by
+  sorry
+  done
+
+example {k : ℕ} : k ^ 2 ≤ 6 ↔ k = 0 ∨ k = 1 ∨ k = 2 := by
+  sorry
+  done
