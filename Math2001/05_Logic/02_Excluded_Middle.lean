538c41899c4d16efe540380f0fe859eb1971d310
Riccardo Brasca <riccardo.brasca@gmail.com>
2025-03-02 11:40:41 +0100

    add file

---
+++ Math2001/05_Logic/02_Excluded_Middle.lean
@@ -0,0 +1,17 @@
+/- Copyright (c) Heather Macbeth, 2023.  All rights reserved. -/
+import Mathlib.Data.Real.Basic
+import Library.Basic
+
+math2001_init
+
+example {P : Prop} (hP : ¬¬P) : P := by
+  by_cases hP : P
+  · apply hP
+  · contradiction
+  done
+
+/-! # Exercises -/
+
+example (P Q : Prop) : (¬P → ¬Q) ↔ (Q → P) := by
+  sorry
+  done
