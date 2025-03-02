732ae8aaf566ed3ae6a127fb065e4c1e4b8740dd
Riccardo Brasca <riccardo.brasca@gmail.com>
2025-03-02 13:39:01 +0100

    better

--- Math2001/06_Induction/02_Recurrence_Relations.lean
+++ Math2001/06_Induction/02_Recurrence_Relations.lean
@@ -10,32 +10,13 @@ namespace Int
 
 def a (n : ℕ) : ℕ := 2 ^ n
 
-
 #eval a 20 -- infoview displays `1048576`
-
 
 def b : ℕ → ℤ
   | 0 => 3
   | n + 1 => b n ^ 2 - 2
 
-
 #eval b 7 -- infoview displays `316837008400094222150776738483768236006420971486980607`
-
-
-def x : ℕ → ℤ
-  | 0 => 5
-  | n + 1 => 2 * x n - 1
-
-example (n : ℕ) : x n = 2 ^ (n + 2) + 1 := by
-  simple_induction n with k IH
-  · -- base case
-    calc x 0 = 5 := by rw [x]
-      _ = 2 ^ (0 + 2) + 1 := by numbers
-  · -- inductive step
-    calc x (k + 1) = 2 * x k - 1 := by rw [x]
-      _ = 2 * (2 ^ (k + 2) + 1) - 1 := by rw [IH]
-      _ = 2 ^ ((k + 1) + 2) + 1 := by ring
-  done
 
 def A : ℕ → ℚ
   | 0 => 0
@@ -53,59 +34,13 @@ example (n : ℕ) : A n = n * (n + 1) / 2 := by
       _ = (k + 1) * (k + 1 + 1) / 2 := by ring
   done
 
-def factorial : ℕ → ℕ
-  | 0 => 1
-  | n + 1 => (n + 1) * factorial n
-
-notation:10000 n "!" => factorial n
-
-example (n : ℕ) : (n + 1)! ≥ 2 ^ n := by
-  sorry
-  done
-
 /-! # Exercises -/
 
-
-def c : ℕ → ℤ
-  | 0 => 7
-  | n + 1 => 3 * c n - 10
-
-example (n : ℕ) : c n = 2 * 3 ^ n + 5 := by
-  sorry
-  done
-
-def y : ℕ → ℕ
-  | 0 => 2
-  | n + 1 => (y n) ^ 2
-
-example (n : ℕ) : y n = 2 ^ (2 ^ n) := by
-  sorry
-  done
 
 def B : ℕ → ℚ
   | 0 => 0
   | n + 1 => B n + (n + 1 : ℚ) ^ 2
 
 example (n : ℕ) : B n = n * (n + 1) * (2 * n + 1) / 6 := by
-  sorry
-  done
-
-def S : ℕ → ℚ
-  | 0 => 1
-  | n + 1 => S n + 1 / 2 ^ (n + 1)
-
-example (n : ℕ) : S n = 2 - 1 / 2 ^ n := by
-  sorry
-  done
-
-example (n : ℕ) : 0 < n ! := by
-  sorry
-  done
-
-example {n : ℕ} (hn : 2 ≤ n) : Nat.Even (n !) := by
-  sorry
-  done
-
-example (n : ℕ) : (n + 1) ! ≤ (n + 1) ^ n := by
   sorry
   done
