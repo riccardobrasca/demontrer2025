83ced7dec7ca70b8f90d36c2455503324dd04628
Riccardo Brasca <riccardo.brasca@gmail.com>
2025-03-02 11:49:01 +0100

    clean

--- Math2001/06_Induction/01_Induction.lean
+++ Math2001/06_Induction/01_Induction.lean
@@ -19,39 +19,6 @@ example (n : ℕ) : 2 ^ n ≥ n + 1 := by
       _ ≥ k + 1 + 1 := by extra
   done
 
-
-example (n : ℕ) : Even n ∨ Odd n := by
-  simple_induction n with k IH
-  · -- base case
-    sorry
-  · -- inductive step
-    obtain ⟨x, hx⟩ | ⟨x, hx⟩ := IH
-    · sorry
-    · sorry
-  done
-
-example {a b d : ℤ} (h : a ≡ b [ZMOD d]) (n : ℕ) : a ^ n ≡ b ^ n [ZMOD d] := by
-  sorry
-  done
-
-example (n : ℕ) : 4 ^ n ≡ 1 [ZMOD 15] ∨ 4 ^ n ≡ 4 [ZMOD 15] := by
-  simple_induction n with k IH
-  · -- base case
-    left
-    numbers
-  · -- inductive step
-    obtain hk | hk := IH
-    · right
-      calc (4:ℤ) ^ (k + 1) = 4 * 4 ^ k := by ring
-        _ ≡ 4 * 1 [ZMOD 15] := by rel [hk]
-        _ = 4 := by numbers
-    · left
-      calc (4:ℤ) ^ (k + 1) = 4 * 4 ^ k := by ring
-        _ ≡ 4 * 4 [ZMOD 15] := by rel [hk]
-        _ = 15 * 1 + 1 := by numbers
-        _ ≡ 1 [ZMOD 15] := by extra
-  done
-
 example {n : ℕ} (hn : 2 ≤ n) : (3:ℤ) ^ n ≥ 2 ^ n + 5 := by
   induction_from_starting_point n, hn with k hk IH
   · -- base case
@@ -63,8 +30,7 @@ example {n : ℕ} (hn : 2 ≤ n) : (3:ℤ) ^ n ≥ 2 ^ n + 5 := by
       _ ≥ 2 ^ (k + 1) + 5 := by extra
   done
 
-example : forall_sufficiently_large n : ℕ, 2 ^ n ≥ n ^ 2 := by
-  dsimp
+example : ∃ (k : ℕ), ∀ n > k, 2 ^ n ≥ n ^ 2 := by
   use 4
   intro n hn
   induction_from_starting_point n, hn with k hk IH
@@ -86,38 +52,14 @@ example {a : ℝ} (ha : -1 ≤ a) (n : ℕ) : (1 + a) ^ n ≥ 1 + n * a := by
   sorry
   done
 
-example (n : ℕ) : 5 ^ n ≡ 1 [ZMOD 8] ∨ 5 ^ n ≡ 5 [ZMOD 8] := by
+example : ∃ (k : ℕ), ∀ n > k, 3 ^ n ≥ 2 ^ n + 100 := by
   sorry
   done
 
-example (n : ℕ) : 6 ^ n ≡ 1 [ZMOD 7] ∨ 6 ^ n ≡ 6 [ZMOD 7] := by
+example : ∃ (k : ℕ), ∀ n > k, 2 ^ n ≥ n ^ 2 + 4 := by
   sorry
   done
 
-example (n : ℕ) :
-    4 ^ n ≡ 1 [ZMOD 7] ∨ 4 ^ n ≡ 2 [ZMOD 7] ∨ 4 ^ n ≡ 4 [ZMOD 7] := by
-  sorry
-  done
-
-example : forall_sufficiently_large n : ℕ, (3:ℤ) ^ n ≥ 2 ^ n + 100 := by
-  dsimp
-  sorry
-  done
-
-example : forall_sufficiently_large n : ℕ, 2 ^ n ≥ n ^ 2 + 4 := by
-  dsimp
-  sorry
-  done
-
-example : forall_sufficiently_large n : ℕ, 2 ^ n ≥ n ^ 3 := by
-  dsimp
-  sorry
-  done
-
-theorem Odd.pow {a : ℕ} (ha : Odd a) (n : ℕ) : Odd (a ^ n) := by
-  sorry
-  done
-
-theorem Nat.even_of_pow_even {a n : ℕ} (ha : Even (a ^ n)) : Even a := by
+example : ∃ (k : ℕ), ∀ n > k, 2 ^ n ≥ n ^ 3 := by
   sorry
   done
