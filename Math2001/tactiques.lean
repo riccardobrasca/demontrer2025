60bd7b94bc348e3af446677e6fd296e747444b24
Riccardo Brasca <riccardo.brasca@gmail.com>
2025-04-02 14:23:19 +0200

    file

---
+++ Math2001/tactiques.lean
@@ -0,0 +1,66 @@
+import Mathlib.Data.Real.Basic
+import Library.Basic
+
+/-
+
+`calc`
+
+`rw`
+
+`ring`
+
+`rel`
+
+`numbers`
+
+`extra`
+
+`addarith`
+
+`have`
+
+`cancel`
+
+`apply`
+
+`obtain`, pour ∨
+
+`left`
+
+`right`
+
+`obtain`, pour ∧
+
+`constructor`, pour ∧
+
+`contradiction` (avec la variante `numbers at`)
+
+`obtain`, pour ∃
+
+`use`
+
+`assumption`
+
+`intro`
+
+`constructor`, pour ↔
+
+`rw`, pour ↔
+
+`interval_cases`
+
+`by_contra'`
+
+`by_cases`
+
+`simple_induction`
+
+`induction_from_starting_point`
+
+`push_neg`
+
+`dsimp`
+
+`ext`
+
+-/
